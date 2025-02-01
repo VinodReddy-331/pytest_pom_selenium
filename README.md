3c7f336349e5500c385229f5d6fc9e29d7a45d9d
Vinod Reddy <nutanakalvavinodkumar@gmail.com>
2025-02-01 21:30:45 +0530

    Allure Report Integration with Pytest Framwork

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # pytest_pom_selenium
 Pytest project for Selenium using POM framework
 
+Pre-requisites:
+python --version
+Python 3.9.10
+allure --version
+2.32.0
+
+
 **create requirement file
 **create virtualenvironment with below steps
 python -m venv <virtual_environment_name>
        -m venv to run venv module(i.e kind of argument for python to invoke venv module)
 <virtual_environment_name>\Scripts\activate
 pip install -r <requirementfile.txt>
+
+
+Download Allure and add the bin directory to the environment variables
+https://allurereport.org/docs/install-for-windows/
--- tests/test_login_functionality.py
+++ tests/test_login_functionality.py
@@ -5,11 +5,15 @@ from selenium.webdriver.common.by import By
 from src.pages.loginpage import LoginPage
 
 
+# pytest.skip("Skip all the testcases")
 class TestLoginFunctionality:
     def test_validate_title(self, browser):
         title = browser.title
         assert title == "GitHub · Build and ship software on a single, collaborative platform · GitHub", f"Title is not matching"
 
+    # @pytest.mark.skip is decorator which is used to skip any testcase without actually removing from suite
+    # Also we can specify the reason why the test case is skip using "reason"
+    @pytest.mark.skip(reason="Not required to test Negative functionality")
     def test_invalidate_signin(self, browser):
         self.loginObj = LoginPage(browser)
         self.loginObj.signup_button()
@@ -26,5 +30,3 @@ class TestLoginFunctionality:
         self.loginObj.enter_password()
         self.loginObj.click_signin()
         assert browser.title == "GitHub"
-
-
